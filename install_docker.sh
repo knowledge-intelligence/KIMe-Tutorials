60c43c492be3835b6a1aaa495da203bc6ec080fd
Sungtae Shin <stshin@dau.ac.kr>
2024-02-16 09:44:21 +0900

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Gen @ 230109
+# Gen @ 240216
 # : Install Docker
 
 set -x
@@ -9,32 +9,27 @@ set -x
 
 #Uninstall old versions
 echo "[Uninstall old versions]"
-sudo apt remove docker docker-engine docker.io containerd runc
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
 
 #Set up the repository
 echo "[Set up the repository]"
-sudo apt update
-sudo apt install -y \
-  ca-certificates \
-  curl \
-  gnupg \
-  lsb-release 
-
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-
-sh -c "echo \
-  \"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable\" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null"
-
-
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
+# Add Docker's official GPG key:
 sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
 
 # Install Docker
 echo "[Install Docker]"
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Hello Docker
 echo "[Hello Docker]"
